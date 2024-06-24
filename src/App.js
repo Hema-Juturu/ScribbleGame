981ef6a559615a376f70abc82d2f5d67fa185a59
Hema-Juturu <jhema7834@gmail.com>
2024-06-24 22:18:11 +0530

    drawing canvas

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Dcanvas from './Dcanvas';
+function App() {
+  return (
+    <div className="App">
+      <Dcanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ColorSelector.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ColorSelector = ({ selectedColor, onColorChange }) => {
+  const handleChange = (event) => {
+    onColorChange(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="colorPicker">Choose a color:</label>
+      <input
+        type="color"
+        id="colorPicker"
+        value={selectedColor}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default ColorSelector;
