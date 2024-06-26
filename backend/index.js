ba3932624efbe75a7f72d77d6d2d11b94e11d19b
Sandeep <bsandeep@corp.untd.com>
2024-06-26 12:15:56 +0530

    added production commands

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
+
 dotenv.config()
 
 const app = express()
@@ -14,5 +16,11 @@ app.get('/api', (_, res) => {
         message: 'Api [scribble]'
     })
 })
+
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (_, res) =>
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+)
 
 app.listen(PORT, () => console.log(`visit http://${HOST}:${PORT}`))
